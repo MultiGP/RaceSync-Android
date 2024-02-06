af58ccec978b9be8444ecf49354cdbb4ce922b9a
Viki Baarathi <viki.baarathi@gmail.com>
2024-02-07 07:25:01 +0800

    Added items to be included in drawer. Not linked yet

--- app/src/main/java/com/multigp/racesync/navigation/NavDestinations.kt
+++ app/src/main/java/com/multigp/racesync/navigation/NavDestinations.kt
@@ -41,4 +41,43 @@ object TrackDesign : NavDestination {
 }
 
 
-val drawerMenu = listOf(Landing, TrackDesign)
+object ObstaclesBuildGuide : NavDestination {
+    override val icon = Icons.Default.Home
+    override val title = "Obstacles Build Guide"
+    override val route = "track"
+}
+
+object RulesRegulation : NavDestination {
+    override val icon = Icons.Default.Home
+    override val title = "Season Rules & Regulations"
+    override val route = "track"
+}
+
+object VisitMultiGPShop : NavDestination {
+    override val icon = Icons.Default.Home
+    override val title = "Visit the MultiGP Shop"
+    override val route = "track"
+}
+
+object VisitMultiGP : NavDestination {
+    override val icon = Icons.Default.Home
+    override val title = "Go to MultiGP.com"
+    override val route = "track"
+}
+
+object Logout : NavDestination {
+    override val icon = Icons.Default.Home
+    override val title = "Logout"
+    override val route = "track"
+}
+
+object SendFeedback : NavDestination {
+    override val icon = Icons.Default.Home
+    override val title = "Send Feedback"
+    override val route = "track"
+}
+
+
+
+
+val drawerMenu = listOf(Landing, TrackDesign, ObstaclesBuildGuide, RulesRegulation, VisitMultiGPShop, VisitMultiGP, SendFeedback, Logout)
--- app/src/main/java/com/multigp/racesync/screens/landing/LandingScreen.kt
+++ app/src/main/java/com/multigp/racesync/screens/landing/LandingScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -102,6 +103,16 @@ fun DrawerContent(
                     onClick = { onMenuClick(it.route) })
                 Divider(thickness = 1.dp, color = MaterialTheme.colorScheme.surfaceVariant)
             }
+
+            Spacer(modifier = modifier.height(16.dp))
+
+            Text(
+                text = "Copyright @ 2015 - 2024 MultiGP, Inc.",
+                color = Color.Gray,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = modifier.align(Alignment.Start),
+
+                )
         }
     }
 }
