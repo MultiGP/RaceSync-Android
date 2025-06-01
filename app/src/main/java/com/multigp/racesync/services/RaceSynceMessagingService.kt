bca3d719951798d9cdb578745e90d7f23a97466e
Viki Baarathi <viki.baarathi@gmail.com>
2025-06-01 21:55:00 +0800

    Updated to receive payload which allows data

--- app/src/main/java/com/multigp/racesync/services/RaceSynceMessagingService.kt
+++ app/src/main/java/com/multigp/racesync/services/RaceSynceMessagingService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_DEFAULT
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.TaskStackBuilder
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
 import android.os.Build
@@ -20,40 +21,44 @@ class RaceSynceMessagingService : FirebaseMessagingService() {
 
     private val random = Random
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        remoteMessage.notification?.let { message ->
-            sendNotification(message)
+        val data = remoteMessage.data
+        if (data.isNotEmpty()) {
+            val title = data["title"] ?: "RaceSync"
+            val body = data["body"] ?: ""
+            val raceID = data["raceId"] ?: return
+            sendNotification(title, body, raceID)
         }
     }
 
-    private fun sendNotification(message: RemoteMessage.Notification) {
-        val raceID = "2057"
+
+    private fun sendNotification(title: String, body: String, raceID: String) {
         val deepLinkUri = "racesync://notification_race_details/$raceID".toUri()
         val intent = Intent(Intent.ACTION_VIEW, deepLinkUri).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TOP
         }
-        val pendingIntent = androidx.core.app.TaskStackBuilder.create(this).run {
+
+        val pendingIntent = TaskStackBuilder.create(this).run {
             addNextIntentWithParentStack(intent)
             getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE)
         }
 
-        val channelId = this.getString(R.string.default_notification_channel_id)
-
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setContentTitle(message.title)
-            .setContentText(message.body)
+        val channelId = getString(R.string.default_notification_channel_id)
+        val builder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
             .setAutoCancel(true)
             .setContentIntent(pendingIntent)
 
         val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(channelId, CHANNEL_NAME, IMPORTANCE_DEFAULT)
             manager.createNotificationChannel(channel)
         }
 
-        manager.notify(random.nextInt(), notificationBuilder.build())
+        manager.notify(Random.nextInt(), builder.build())
     }
+
 
     override fun onNewToken(token: String) {
         // If you want to send messages to this application instance or
